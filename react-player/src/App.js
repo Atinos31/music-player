af0919ccac7dd80b84f60f1dd52c64b6827b080a
Atinos31 <atinosandra2014@gmail.com>
2022-04-08 17:56:00 +0000

    adding song data & useState

--- react-player/src/App.js
+++ react-player/src/App.js
@@ -1,15 +1,21 @@
 
-import React from 'react'
+import React, {useState} from 'react'
 // import styles
 import './styles/app.scss'
 // adding components
+
+// import util
+import data from './util'
 import Player from './components/Player';
 import Song from './components/Song';
 
 function App() {
+  // state
+  const [songs, setSongs] = useState (data());
+  const [currentSong, setCurrentSongs] = useState (songs[0]);
   return (
     <div className="App">
-    <Song />
+    <Song  currentSong={currentSong} />
     <Player />
     </div>
   );
